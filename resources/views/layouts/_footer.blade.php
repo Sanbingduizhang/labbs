a0d2e118d013d9dfe0ee8a8501e32f64ff16d922
root <root@iZ2ze167an2zld6lqj4enzZ.(none)>
2019-03-06 17:44:57 +0800

    更新

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            由 <a href="#">李小文</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+        <p class="float-right"><a href="#"> | 联系我们</a></p>
+        <p class="float-right"><a href="#">参照(L02 Laravel 教程 - Web 开发实战进阶) </a></p>
+    </div>
+</footer>
---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
+    <div class="container">
+        <!-- Branding Image -->
+        <a class="navbar-brand" href="{{ url('/') }}">
+            labbs
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+            <!-- left side of navbar -->
+            <ul class="navbar-nav mr-auto">
+
+            </ul>
+
+            <!-- right side of navbar -->
+            <ul class="navbar-nav navbar-right">
+                <!-- auth links -->
+                <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
